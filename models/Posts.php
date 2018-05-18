dfbd960ab95253f91a92bbe785c6943ba95a7a7d
VladimirAf <ks0409411@yandex.ru>
2018-05-18 13:20:35 +0300

    testask is performed

---
+++ models/Posts.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "posts".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $published_at
+ * @property string $content
+ */
+class Posts extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'posts';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'published_at', 'content'], 'required'],
+            [['published_at'], 'safe'],
+            [['content'], 'string'],
+            [['title'], 'string', 'max' => 155],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'published_at' => 'Published At',
+            'content' => 'Content',
+        ];
+    }
+}
